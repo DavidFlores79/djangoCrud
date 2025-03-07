f40280123a6038c4e597e5d052672d33a57c2e55
David Flores <da.flores@financialgroup.mx>
2025-03-07 12:47:38 -0600

    Settings.py

--- simple_crud/settings.py
+++ simple_crud/settings.py
@@ -130,7 +130,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# if DEBUG:
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
