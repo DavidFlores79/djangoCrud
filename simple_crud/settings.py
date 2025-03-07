e66b470ae419a40e4138ee347aab81d7aab59643
David Flores <da.flores@financialgroup.mx>
2025-03-07 12:43:08 -0600

    Default sqlite

--- simple_crud/settings.py
+++ simple_crud/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'simple_crud.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default=os.getenv('DATABASE_URL'),
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
